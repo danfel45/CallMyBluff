40aa606456de9a5989de765742b478d76aab2948
danfel45 <65387779+danfel45@users.noreply.github.com>
2025-02-04 17:12:44 -0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokerTestAng';
+  cards: { rank: string, suit: string, selected: boolean }[] = [];
+  selectedHandCards: { rank: string, suit: string, selected: boolean }[] = [];
+  selectedRiverCards: { rank: string, suit: string, selected: boolean }[] = [];
+  handCards: { rank: string, suit: string, selected: boolean }[] = [];
+  riverCards: { rank: string, suit: string, selected: boolean }[] = [];
+
+  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+  ngOnInit() {
+    this.generateDeck();
+  }
+
+  generateDeck() {
+    for (let suit of this.suits) {
+      for (let rank of this.ranks) {
+        this.cards.push({ rank, suit, selected: false });
+      }
+    }
+  }
+
+  toggleCardSelection(card: { rank: string, suit: string, selected: boolean }) {
+    if (card.selected) {
+      // console.log("Deselected! " + card.rank + card.suit);
+      card.selected = false;
+      this.selectedHandCards = this.selectedHandCards.filter(c => c !== card);
+      this.selectedRiverCards = this.selectedRiverCards.filter(c => c !== card);
+    } else {
+      if (this.selectedHandCards.length < 2) {
+        card.selected = true;
+        this.selectedHandCards.push(card);
+      } else if (this.selectedRiverCards.length < 5) {
+        card.selected = true;
+        this.selectedRiverCards.push(card);
+      }
+    }
+  }
+
+  
 }
