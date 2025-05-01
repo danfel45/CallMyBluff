62ef36698a57347df904e1674b35af0a6f2fb46c
pissedasian <157274643+pissedasian@users.noreply.github.com>
2025-04-30 17:04:10 -0700

    Updated poker.component.ts

--- src/app/poker/poker.component.ts
+++ src/app/poker/poker.component.ts
@@ -11,7 +11,7 @@ import { CardComponent } from '../card/card.component';
 })
 export class PokerComponent {
   suits = ['h', 'd', 'c', 's'];
-  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   deck: string[] = [];
   hand: string[] = [];
   community: string[] = [];
